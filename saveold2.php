84f294823973d734b8252ca596c5a140294ceee7
tutoro <twmr1980@gmail.com>
2018-05-21 21:51:33 -0500

    updated readme tonight

--- saveold2.php
+++ saveold2.php
@@ -24,24 +24,27 @@ if(is_writeable("invupd.log")){
 	$countInvLog = count($readInvLog);
 	//convert barcode to item id reconizable by levels table
 	$sqlID = "SELECT id FROM stored_items WHERE code = '".$itemId."'"; 
+	//$resultID = $link->query($sqlID);
 	$res = mysqli_fetch_row($link->query($sqlID));
+	//$barToId = $res[0];
 	//variable to set if select found a real item
 	$countRes = count($res);
     //select item name using barcode scanned and converted to item id
 	$sqlItemName = "SELECT name FROM stored_items WHERE id = '".$res[0]."'";
-	$itemName = mysqli_fetch_row($link->query($sqlItemName));
-	$displayName = $itemName[0];
+	//$resultItemName = $link->query($sqlItemName);
+	$res2 = mysqli_fetch_row($link->query($sqlItemName));
+	$displayName = $res2[0];
 	//updates level based on id converted from barcode select, then log it
 	$updLevel = "UPDATE stock_levels SET stocklevel = stocklevel + " . $amountAdj . " WHERE storeditemid = '" . $res[0] . "'";
 	//update stock level and log it in the log file.  if no item was found under scanned code, log the error
 	if ($link->query($updLevel) === TRUE) {
 		if($countRes == 1) {
 			$tStamp = date('Y-m-d h:i:sa');
-			$logTxt = ($countInvLog+1).". ". $itemName[0] .", ".$itemId.", ". $amountAdj.", ". $tStamp. ",  ".$res[0].$countRes."\n";
+			$logTxt = ($countInvLog+1).". ". $displayName .", ".$itemId.", ". $amountAdj.", ". $tStamp. ",  ".$countRes. $res[0]."\n";
 			fwrite($fh, $logTxt);
 		} elseif ($countRes == 0){
 			$tStamp = date('Y-m-d h:i:sa');
-			$logTxt = ($countInvLog+1).". Error - Does not exist: ".$itemId.", ". $amountAdj.", ". $tStamp. ",  ".$res[0]."\n";
+			$logTxt = ($countInvLog+1).". Error - Does not exist: ".$itemId.", ". $amountAdj.", ". $tStamp. ",  ".$countRes. $res[0]."\n";
 			fwrite($fh, $logTxt);
 		} //add code for duplicate numbers
 	} else {
